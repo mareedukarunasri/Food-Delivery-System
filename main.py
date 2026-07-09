c0b8575f284ae90169a4cab54141a67138677360
karunasri <mareedukarunasri@gmail.com>
2026-07-09 21:57:33 +0530

    Week 1 Completed

---
+++ main.py
@@ -0,0 +1,15 @@
+print("=" * 40)
+print("      FOOD DELIVERY SYSTEM")
+print("=" * 40)
+
+print("Welcome to Food Delivery System")
+
+print("\nModules")
+print("1. User Registration")
+print("2. Login")
+print("3. Restaurant")
+print("4. Food Menu")
+print("5. Cart")
+print("6. Payment")
+
+print("\nProject Started Successfully!")
