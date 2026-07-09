0f13a0d3d9772366d6d1853e3c5d9a824e4477a9
karunasri <mareedukarunasri@gmail.com>
2026-07-09 23:10:21 +0530

    Week 3: Added User Registration and Login modules

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from src.user import register
+from src.login import login
 
 while True:
     print("\n===== FOOD DELIVERY SYSTEM =====")
     print("1. User Registration")
-    print("2. Exit")
+    print("2. Login")
+    print("3. Exit")
 
     choice = input("Enter your choice: ")
 
     if choice == "1":
         register()
     elif choice == "2":
-        print("Thank you!")
+        login()
+    elif choice == "3":
+        print("Thank you for using the Food Delivery System!")
         break
     else:
         print("Invalid Choice")
---
+++ src/login.py
@@ -0,0 +1,8 @@
+def login():
+    print("\n===== LOGIN =====")
+
+    email = input("Enter Email: ")
+    password = input("Enter Password: ")
+
+    print("\nLogin Successful!")
+    print("Welcome,", email)
